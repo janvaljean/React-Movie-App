ecffd265c053a923e3a96f6057a97525785690a6
janvaljean <kurtuldu@gmx.de>
2023-03-14 14:34:21 +0100

    auth context updated

--- movie-app/src/context/AuthContext.js
+++ movie-app/src/context/AuthContext.js
@@ -1,5 +1,12 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import React, { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../auth/firebase";
 
 export const AuthContext = createContext();
@@ -9,22 +16,75 @@ export const AuthContext = createContext();
 //     return useContext(AuthContext);
 //   };
 
-const createUser = async (email, password) => {
-  try {
-    //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    console.log(userCredential);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const AuthContextProvider = ({ children }) => {
-  const values = { createUser, currentUser: { displayName: "felix franko" } };
+  const [currentUser, setCurrentUser] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    userObserver();
+  }, []);
+
+  const createUser = async (email, password, displayName) => {
+    try {
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      //? kullanıcı profilini güncellemek için kullanılan firebase metodu
+      await updateProfile(auth.currentUser, {
+        displayName: displayName,
+      });
+      navigate("/");
+      //   console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+  const signIn = async (email, password) => {
+    try {
+      //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+      let userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      navigate("/");
+      //   console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  const userObserver = () => {
+    //? Kullanıcının signin olup olmadığını takip eden ve kullanıcı değiştiğinde yeni kullanıcıyı response olarak dönen firebase metodu
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        const { email, displayName, photoURL } = user;
+        setCurrentUser({ email, displayName, photoURL });
+      } else {
+        setCurrentUser(false);
+        console.log("user logged out");
+      }
+    });
+  };
+
+  const values = {
+    createUser,
+    signIn,
+    logOut,
+    currentUser,
+  };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
