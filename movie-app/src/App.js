ca3d96745111dd09d5e24dbe26f280ec7aaae78c
janvaljean <kurtuldu@gmx.de>
2023-03-17 14:10:31 +0100

    movie provider was added to app

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
  import { ToastContainer, toast } from 'react-toastify';
+import { MovieProvider } from "./context/MovieContext";
 
 const App = () => {
   return (
     <div className="bg-gray-dark-main">
       <AuthContextProvider>
-        <AppRouter />
-         <ToastContainer/>
+        <MovieProvider>
+          <AppRouter />
+          <ToastContainer/>
+        </MovieProvider>
       </AuthContextProvider>
     </div>
   );
