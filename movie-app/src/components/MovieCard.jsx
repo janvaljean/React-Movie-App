113618e4ad3c28dcee8dca317d3de0881accb110
janvaljean <kurtuldu@gmx.de>
2023-03-21 01:52:48 +0100

    AppRouter and PrivatRouter

--- movie-app/src/components/MovieCard.jsx
+++ movie-app/src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
@@ -8,6 +9,7 @@ const defaultImage =
 
 const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
     const{currentUser} = useContext(AuthContext )
+    const navigate = useNavigate()
     function getVoteClass(vote){
       if(vote >= 8){
         return "green"
@@ -19,7 +21,7 @@ const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
 
     }
   return (
-    <div className='movie'>
+    <div className='movie' onClick={() => navigate("/details/"+id)}>
       <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="movie-card" loading='lazy' />
       <div className="flex align-baseline justify-between p-1 text-white">
         <h5>{title}</h5>
---
+++ movie-app/src/components/VideoSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoSection = ({videoKey}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VideoSection
