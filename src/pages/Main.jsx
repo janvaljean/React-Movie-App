973fb01de5435cbeb48a14afb0c1fc0578419c46
janvaljean <kurtuldu@gmx.de>
2023-04-26 19:57:38 +0200

    main updated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 import { AuthContext } from "../context/AuthContext";
 import { MovieContext } from "../context/MovieContext";
